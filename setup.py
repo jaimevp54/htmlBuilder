4b0135015c8d6eaa13cc109d71978e1c10ff54d6
jaimevp54 <jaimevp54@gmail.com>
2017-07-03 12:08:20 -0400

    [added] added al valid html5 attribute tags [added] attribute class checking for attributes

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from distutils.core import setup
 setup(
     name='htmlBuilder',
     packages=['htmlBuilder'],
-    version='0.1.1',
+    version='0.1.2',
     description='A beautiful html builder library.',
     author='Jaime Viñas',
     author_email='jaimevp54@gmail.com',
+    license="MIT",
     url='https://github.com/jaimevp54/htmlBuilder',
     download_url='https://github.com/jaimevp54/htmlBuilder/archive/0.1.1.tar.gz',
     keywords=['hmtl', 'builder', 'render'],
-    classifiers=[],
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
 )
