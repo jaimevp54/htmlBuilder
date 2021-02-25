88811475a208d67c89205e995eca9e205ab860ca
jaimevp54 <jaimevp54@gmail.com>
2021-02-24 20:27:37 -0500

    added tuple as an allowed iterable for utils.flatten_params

--- htmlBuilder/utils.py
+++ htmlBuilder/utils.py
@@ -1,7 +1,7 @@
 def flatten_params(params):
     result = []
     for item in params:
-        if isinstance(item, list):
+        if isinstance(item, (list, tuple)):
             for element in flatten_params(item):
                 result.append(element)
         else:
