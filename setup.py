4b150e4528523c1af3b29a6f10b0593c8d70f5d5
Jaime Viñas <jaimevp54@gmail.com>
2021-02-26 02:46:22 -0500

    Updated version to 0.2.1

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='htmlBuilder',
     packages=['htmlBuilder'],
-    version='0.1.2',
+    version='0.2.1',
     description='A beautiful html builder library.',
     author='Jaime Viñas',
     author_email='jaimevp54@gmail.com',
     license="MIT",
     url='https://github.com/jaimevp54/htmlBuilder',
-    download_url='https://github.com/jaimevp54/htmlBuilder/archive/0.1.2.tar.gz',
+    download_url='https://github.com/jaimevp54/htmlBuilder/archive/0.2.1.tar.gz',
     keywords=['hmtl', 'builder', 'render'],
     python_requires='>=3.6',
     classifiers=[
