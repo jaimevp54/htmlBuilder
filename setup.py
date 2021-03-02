1764048edf10982ffa5bd0f1d61a228af49fd348
jaimevp54 <jaimevp54@gmail.com>
2021-03-01 19:59:31 -0500

    Updated setup.py to include more accurate information

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+_VERSION = '0.2.1'
+
 setup(
     name='htmlBuilder',
     packages=['htmlBuilder'],
-    version='0.2.1',
+    version=_VERSION,
     description='A beautiful html builder library.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jaime Viñas',
     author_email='jaimevp54@gmail.com',
     license="MIT",
     url='https://github.com/jaimevp54/htmlBuilder',
-    download_url='https://github.com/jaimevp54/htmlBuilder/archive/0.2.1.tar.gz',
-    keywords=['hmtl', 'builder', 'render'],
+    download_url=f'https://github.com/jaimevp54/htmlBuilder/archive/{_VERSION}.tar.gz',
+    keywords=['html', 'builder', 'render', 'template', 'templating', 'python'],
     python_requires='>=3.6',
     classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: HTML',
     ],
 )
